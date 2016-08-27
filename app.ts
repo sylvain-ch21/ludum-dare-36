7acd98d51d420739c0fdb51d6d27f832e34f3d9a
Wheerd <admin@wheerd.de>
2016-08-27 13:49:13 +0200

    More tiles and a test map.

--- app.ts
+++ app.ts
@@ -1,6 +1,12 @@
 /// <reference path="bower_components/phaser/typescript/phaser.d.ts" />
 // import * as Phaser from 'phaser'
 
+const TOP = 1;
+const BOTTOM = 2;
+const LEFT = 4;
+const RIGHT = 8;
+const IMAGE_FOLDER = 'images/'
+
 module GameFromScratch {
     export class TitleScreenState extends Phaser.State {
         game: Phaser.Game;
@@ -10,36 +16,38 @@ module GameFromScratch {
         titleScreenImage: Phaser.Sprite;
 
         preload() {
-            this.load.image("title", "TitleScreen.png");
+            /*
+            ['aqueduct', 'aqueduct-water'].forEach(type => {
+                for (var i = 0; i < 16; i++) {
+                    var suffix = ''
+                    if (i & TOP) suffix += 'T'
+                    if (i & BOTTOM) suffix += 'B'
+                    if (i & LEFT) suffix += 'L'
+                    if (i & RIGHT) suffix += 'R'
+                    if (suffix.length < 2) continue
+                    var name = type + '-' + suffix + '.png'
+                    this.load.image(type + i, IMAGE_FOLDER + name);
+                }
+            })
+            */
+            this.load.tilemap('test', 'maps/test.json', null, Phaser.Tilemap.TILED_JSON);
+            this.load.image('tiles', IMAGE_FOLDER + 'tiles.png');
         }
         create() {
-            this.titleScreenImage = this.add.sprite(0, 0, "title");
-            this.input.onTap.addOnce(this.titleClicked,this); // <-- that um, this is extremely important
-        }
-        titleClicked (){
-            this.game.state.start("GameRunningState");
-        }
-    }
-
-    export class GameRunningState extends Phaser.State {
-        constructor() {
-            super();
-        }
-        textValue: Phaser.Text;
-        updateCount: number;
-
-        create() {
-            var style = { font: "65px Arial", fill: "#ff0000", align: "center" };
-            this.textValue = this.game.add.text(0, 0, "0", style);
-            this.updateCount = 0;
-        }
-
-        update() {
-            this.textValue.text = (this.updateCount++).toString();
-        }
-
-        render() {
-            this.game.debug.text("This is drawn in render()", 0, 80);
+            this.stage.backgroundColor = '#787878';
+            /*
+            for (var x = 100; x < 200; x += 32) {                
+                for (var y = 100; y < 200; y += 28) {
+                    var sprite = this.add.sprite(x, y, "aqueduct15");
+                    sprite.anchor.y = 1/4
+                    console.log(sprite.offsetY)
+                }
+            }
+            */
+            var map = this.add.tilemap('test');
+            map.addTilesetImage('tiles', 'tiles');
+            var layer = map.createLayer('floor');
+            layer.resizeWorld();
         }
     }
 
@@ -49,7 +57,6 @@ module GameFromScratch {
         constructor() {
             this.game = new Phaser.Game(800, 600, Phaser.WEBGL, 'content');
 
-            this.game.state.add("GameRunningState", GameRunningState, false);
             this.game.state.add("TitleScreenState", TitleScreenState, false);
             this.game.state.start("TitleScreenState", true, true);
         }
